159b8fceae3de6048186d16f5c6bf4fe48a9e9bf
Potato <potatodevelopment06@gmail.com>
2024-01-28 19:38:35 -0700

    Initial commit and push

---
+++ src/main/java/dev/potato/tntchallenge/TNTChallenge.java
@@ -0,0 +1,17 @@
+package dev.potato.tntchallenge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTChallenge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
