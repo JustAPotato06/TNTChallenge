c865deadc0f0d4e26a8763ce7cf9d5d2634b9aa8
Potato <potatodevelopment06@gmail.com>
2024-02-28 22:01:10 -0700

    Small setup code changes

--- src/main/java/dev/potato/tntchallenge/listeners/SetupListeners.java
+++ src/main/java/dev/potato/tntchallenge/listeners/SetupListeners.java
@@ -35,34 +35,50 @@ public class SetupListeners implements Listener {
             // Make sure the action is a right click block and that they have the setup stick in hand
             if (e.getAction() == Action.RIGHT_CLICK_BLOCK && p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "" + ChatColor.BOLD + "SETUP STICK")) {
                 // Set the corner according to what the player is currently setting
-                if (currentSet.equalsIgnoreCase("wall1Corner1"))
-                    setCorner(p, playerData, e, "WALL 1 CORNER 1", "WALL 1 CORNER 2", "wall1Corner2");
-                if (currentSet.equalsIgnoreCase("wall1Corner2"))
-                    setCorner(p, playerData, e, "WALL 1 CORNER 2", "WALL 2 CORNER 1", "wall2Corner1");
-                if (currentSet.equalsIgnoreCase("wall2Corner1"))
-                    setCorner(p, playerData, e, "WALL 2 CORNER 1", "WALL 2 CORNER 2", "wall2Corner2");
-                if (currentSet.equalsIgnoreCase("wall2Corner2"))
-                    setCorner(p, playerData, e, "WALL 2 CORNER 2", "WALL 3 CORNER 1", "wall3Corner1");
-                if (currentSet.equalsIgnoreCase("wall3Corner1"))
-                    setCorner(p, playerData, e, "WALL 3 CORNER 1", "WALL 3 CORNER 2", "wall3Corner2");
-                if (currentSet.equalsIgnoreCase("wall3Corner2"))
-                    setCorner(p, playerData, e, "WALL 3 CORNER 2", "WALL 4 CORNER 1", "wall4Corner1");
-                if (currentSet.equalsIgnoreCase("wall4Corner1"))
-                    setCorner(p, playerData, e, "WALL 4 CORNER 1", "WALL 4 CORNER 2", "wall4Corner2");
-                if (currentSet.equalsIgnoreCase("wall4Corner2"))
-                    setCorner(p, playerData, e, "WALL 4 CORNER 2", "FLOOR CORNER 1", "floorCorner1");
-                if (currentSet.equalsIgnoreCase("floorCorner1"))
-                    setCorner(p, playerData, e, "FLOOR CORNER 1", "FLOOR CORNER 2", "floorCorner2");
-                if (currentSet.equalsIgnoreCase("floorCorner2"))
-                    setCorner(p, playerData, e, "FLOOR CORNER 2", "PLACE AREA CORNER 1", "placeAreaCorner1");
-                if (currentSet.equalsIgnoreCase("placeAreaCorner1"))
-                    setCorner(p, playerData, e, "PLACE AREA CORNER 1", "PLACE AREA CORNER 2", "placeAreaCorner2");
-                if (currentSet.equalsIgnoreCase("placeAreaCorner2"))
-                    setCorner(p, playerData, e, "PLACE AREA CORNER 2", "WIN AREA CORNER 1", "winAreaCorner1");
-                if (currentSet.equalsIgnoreCase("winAreaCorner1"))
-                    setCorner(p, playerData, e, "WIN AREA CORNER 1", "WIN AREA CORNER 2", "winAreaCorner2");
-                if (currentSet.equalsIgnoreCase("winAreaCorner2"))
-                    setCorner(p, playerData, e, "WIN AREA CORNER 2", "", "");
+                switch (currentSet) {
+                    case "wall1Corner1":
+                        setCorner(p, playerData, e, "WALL 1 CORNER 1", "WALL 1 CORNER 2", "wall1Corner2");
+                        break;
+                    case "wall1Corner2":
+                        setCorner(p, playerData, e, "WALL 1 CORNER 2", "WALL 2 CORNER 1", "wall2Corner1");
+                        break;
+                    case "wall2Corner1":
+                        setCorner(p, playerData, e, "WALL 2 CORNER 1", "WALL 2 CORNER 2", "wall2Corner2");
+                        break;
+                    case "wall2Corner2":
+                        setCorner(p, playerData, e, "WALL 2 CORNER 2", "WALL 3 CORNER 1", "wall3Corner1");
+                        break;
+                    case "wall3Corner1":
+                        setCorner(p, playerData, e, "WALL 3 CORNER 1", "WALL 3 CORNER 2", "wall3Corner2");
+                        break;
+                    case "wall3Corner2":
+                        setCorner(p, playerData, e, "WALL 3 CORNER 2", "WALL 4 CORNER 1", "wall4Corner1");
+                        break;
+                    case "wall4Corner1":
+                        setCorner(p, playerData, e, "WALL 4 CORNER 1", "WALL 4 CORNER 2", "wall4Corner2");
+                        break;
+                    case "wall4Corner2":
+                        setCorner(p, playerData, e, "WALL 4 CORNER 2", "FLOOR CORNER 1", "floorCorner1");
+                        break;
+                    case "floorCorner1":
+                        setCorner(p, playerData, e, "FLOOR CORNER 1", "FLOOR CORNER 2", "floorCorner2");
+                        break;
+                    case "floorCorner2":
+                        setCorner(p, playerData, e, "FLOOR CORNER 2", "PLACE AREA CORNER 1", "placeAreaCorner1");
+                        break;
+                    case "placeAreaCorner1":
+                        setCorner(p, playerData, e, "PLACE AREA CORNER 1", "PLACE AREA CORNER 2", "placeAreaCorner2");
+                        break;
+                    case "placeAreaCorner2":
+                        setCorner(p, playerData, e, "PLACE AREA CORNER 2", "WIN AREA CORNER 1", "winAreaCorner1");
+                        break;
+                    case "winAreaCorner1":
+                        setCorner(p, playerData, e, "WIN AREA CORNER 1", "WIN AREA CORNER 2", "winAreaCorner2");
+                        break;
+                    case "winAreaCorner2":
+                        setCorner(p, playerData, e, "WIN AREA CORNER 2", "", "");
+                        break;
+                }
             }
         }
     }
